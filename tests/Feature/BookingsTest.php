a78227a8237c461ccd78482baa00fb2c9fe60387
Deuti Gurung <deuti@ininepal.com>
2023-07-25 16:23:22 +0545

    booking test add

---
+++ tests/Feature/BookingsTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Apartment;
+use App\Models\Booking;
+use App\Models\Property;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BookingsTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function create_apartment(){
+        return $apartment1 = Apartment::factory()->create([
+            'name' => 'Large apartment',
+            'property_id'=> Property::value('id'),
+            'capacity_adults' => 3,
+            'capacity_children' => 2,
+        ]);
+    }
+    public function test_user_can_post_rating_for_their_booking(){
+        $user1 = User::factory()->create(['role_id' => Role::USER_ROLE]);
+        $user2 = User::factory()->create(['role_id' => Role::USER_ROLE]);
+        $apartment = $this->create_apartment();
+        $booking = Booking::create([
+            'apartment_id' => $apartment->id,
+            'user_id' => $user1->id,
+            'start_date' => now()->addDay(),
+            'end_date' => now()->addDays(2),
+            'guest_adults' => 1,
+            'guest_children' => 0,
+        ]);
+
+        //case 1: unauthorized access to user2
+        $response = $this->actingAs($user2)->putJson('/api/user/bookings/'.$booking->id,[]);
+        $response->assertStatus(403);
+
+        //case 2: return bad request error ie.rating is between 1-10 and comment is min:20 
+        $response = $this->actingAs($user1)->putJson('/api/user/bookings/'.$booking->id,[
+            "rating" => 50
+        ]);
+        $response->assertStatus(422);
+
+        $response = $this->actingAs($user1)->putJson('/api/user/bookings/' . $booking->id, [
+            'rating' => 10,
+            'review_comment' => 'Too short comment.'
+        ]);
+        $response->assertStatus(422);
+
+        //case 3: right data
+        $data = [
+            'rating' => 10,
+            'review_comment' => 'Very good apartment with best services and features.'
+        ];
+        $response = $this->actingAs($user1)->putJson('/api/user/bookings/' . $booking->id,$data);
+        $response->assertStatus(200);
+        $response->assertJsonFragment($data);
+
+    }
+}
