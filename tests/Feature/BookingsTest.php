226d0c8c54c17f46240b75851aca8316ae08fa7d
Deuti Gurung <deuti@ininepal.com>
2023-07-25 16:34:25 +0545

    booking store test done

--- tests/Feature/BookingsTest.php
+++ tests/Feature/BookingsTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\Apartment;
 use App\Models\Booking;
+use App\Models\City;
 use App\Models\Property;
 use App\Models\Role;
 use App\Models\User;
@@ -24,13 +25,48 @@ class BookingsTest extends TestCase
     }
 
     public function create_apartment(){
-        return $apartment1 = Apartment::factory()->create([
-            'name' => 'Large apartment',
-            'property_id'=> Property::value('id'),
+        $owner = User::factory()->create(['role_id' => Role::OWNER_ROLE]);
+        $city = City::value('id');
+        $property = Property::factory()->create([
+            'owner_id' => $owner->id,
+            'city_id' => $city,
+        ]);
+        return Apartment::factory()->create([
+            'name' => 'Apartment',
+            'property_id'=> $property->id,
             'capacity_adults' => 3,
             'capacity_children' => 2,
         ]);
     }
+
+    public function test_user_can_book_apartment_but_not_twice(){
+        $user1 = User::factory()->create(['role_id' => Role::USER_ROLE]);
+        $apartment = $this->create_apartment();
+        $bookingData = [
+            'apartment_id' => $apartment->id,
+            'user_id' => $user1->id,
+            'start_date' => now()->addDay(),
+            'end_date' => now()->addDays(2),
+            'guest_adults' => 1,
+            'guest_children' => 0,
+        ];
+        //case 1: store user booking information twice with same data
+        $response = $this->actingAs($user1)->postJson('/api/user/bookings/',$bookingData);
+        $response->assertStatus(201);
+
+        $response = $this->actingAs($user1)->postJson('/api/user/bookings/',$bookingData);
+        $response->assertStatus(422);
+
+        //case 2: store same apartment booking  twice even though date is different
+        $bookingData['start_date'] = now()->addDays(3);
+        $bookingData['end_date'] = now()->addDays(4);
+        $bookingData['guests_adults'] = 5;
+        $response = $this->actingAs($user1)->postJson('/api/user/bookings/',$bookingData);
+        $response->assertStatus(201);
+
+    }
+
+
     public function test_user_can_post_rating_for_their_booking(){
         $user1 = User::factory()->create(['role_id' => Role::USER_ROLE]);
         $user2 = User::factory()->create(['role_id' => Role::USER_ROLE]);
